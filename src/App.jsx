80e67c41f24e385892a6cc5797ad8bb14dbde588
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-05-22 16:24:45 -0300

    Requisito 2: concluído

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 import Login from './pages/Login';
+import Trivia from './pages/Trivia';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact to="/" component={ Login } />
+        <Route to="/trivia" component={ Trivia } />
       </Switch>
     );
   }
