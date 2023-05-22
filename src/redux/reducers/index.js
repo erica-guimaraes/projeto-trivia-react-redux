4d08d8006229680ba3808f4712f01a223d951b91
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-05-22 15:59:25 -0300

    Requisito 1: concluído correção

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from "redux";
-import player from "./player";
+import { combineReducers } from 'redux';
+import player from './player';
 
 const rootReducer = combineReducers({ player });
 
