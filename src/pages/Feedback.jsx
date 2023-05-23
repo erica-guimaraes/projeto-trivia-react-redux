197a95d8a871366defb310e2bcb48694bcf1d5d5
Gabriel Amirati <gabriel_amirati@hotmail.com>
2023-05-23 15:49:19 -0300

    Requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
     const { score } = this.props;
+    const mNumber = 3;
     return (
       <div>
         <Header />
-
+        { score < mNumber ? <p>Could be better...</p> : <p>Well Done!</p> }
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  score: PropTypes.number,
+}.isRequired;
 
 const mapStateToProps = ({ player }) => ({
   score: player.score,
