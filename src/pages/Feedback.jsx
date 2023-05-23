104d4c0507ac35ad547c2a99d88b7b85a0c7eee5
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-05-23 17:17:03 -0300

    Requisito 15 concluido

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  state = {
+    loading: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
   render() {
-    const { assertions } = this.props;
+    const { loading } = this.state;
+    const { assertions, score } = this.props;
     const mNumber = 3;
+    if (loading) return <Redirect to="/" />;
     return (
       <div>
         <Header />
@@ -17,6 +30,15 @@ class Feedback extends Component {
             </p>
             : <p data-testid="feedback-text">Well Done!</p>
         }
+        <p data-testid="feedback-total-score">{ score }</p>
+        <p data-testid="feedback-total-question">{ assertions }</p>
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
@@ -24,10 +46,12 @@ class Feedback extends Component {
 
 Feedback.propTypes = {
   assertions: PropTypes.number,
+  score: PropTypes.number,
 }.isRequired;
 
 const mapStateToProps = ({ player }) => ({
   assertions: player.assertions,
+  score: player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
